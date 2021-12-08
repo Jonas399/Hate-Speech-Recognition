6a912b8af562947b84de21ec46655cb60ba38785
felixg97 <felix.gerschner@yahoo.de>
2021-12-08 17:44:07 +0100

    first gui comps

--- gui/gui.py
+++ gui/gui.py
@@ -3,17 +3,16 @@ gui.py to use via GUI
 
 """
 import tkinter as tk
+from tkinter import ttk
 
-
-class Window:
+class GUI():
 
     def __init__(self, window):
-
         self.window = window
 
         # init constants
-        w_width = 1000
-        w_height = 600
+        w_width = 1500
+        w_height = 1100
 
         # set title
         self.window.title('Hate Speech Detection Bot')
@@ -27,21 +26,38 @@ class Window:
 
         # set window geometry and pos @start
         self.window.geometry('%dx%d+%d+%d' % (w_width, w_height, w_x, w_y))
+        self.window.resizable(False, False)
+
+        # container
+        self.container = tk.Frame(self.window, width=w_width, height=w_height)
+        self.container.grid(column=0, row=0, padx=0, pady=0, sticky='nsew')
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+        self.container.pack_propagate(False)
+
+        # nav-menu
+        self.nav_menu = tk.Frame(self.container, bg='#234235', width=w_width/4, height=w_height)
+        self.nav_menu.grid(row=0, column=0, sticky='nsew')
+        self.nav_menu.grid_rowconfigure((0, 2), weight=1)
+        self.nav_menu.grid_rowconfigure(1, weight=5)
+        self.nav_menu.grid_columnconfigure(0, weight=1)
+        self.nav_menu.pack_propagate(False)
+
+        ## button
+        self.filter_input = tk.Entry(self.nav_menu)
+        self.filter_input.insert(1, 'filter conversations...')
+        self.filter_input.grid(column=0, row=1, padx=10, pady=5, sticky='nsew')
 
 
-    def handle_click(self):
-        pass
 
+        ## frame
 
+        # main
+        self.main_content = tk.Frame(self.container, bg='#C4C4C4', width=w_width/4*3, height=w_height)
+        self.nav_menu.grid(row=0, column=0, sticky='nsew')
 
+        self.conversation_window = tk.Frame(self.main_content, bg='#ffffff')
 
+        self.message_window = tk.Frame(self.main_content, bg='#ffffff')
     
     
-
-    
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -2,3 +2,20 @@
 main.py to test via console
 
 """
+
+from gui import GUI
+
+import tkinter as tk
+
+
+
+if __name__ == '__main__':
+
+    # do some starting stuff
+
+
+    # init Frame
+    root = tk.Tk()
+    frame = GUI(root)
+
+    root.mainloop()
