4a484fa03e52660224900afe890fb7ee30b877c3
felixg97 <felix.gerschner@yahoo.de>
2021-12-08 18:54:40 +0100

    frame for start of bot

--- gui/gui.py
+++ gui/gui.py
@@ -27,37 +27,37 @@ class GUI():
         # set window geometry and pos @start
         self.window.geometry('%dx%d+%d+%d' % (w_width, w_height, w_x, w_y))
         self.window.resizable(False, False)
+        self.window.grid_rowconfigure(0, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
 
         # container
-        self.container = tk.Frame(self.window, width=w_width, height=w_height)
+        self.container = tk.Frame(self.window, bg='#ad8088')
         self.container.grid(column=0, row=0, padx=0, pady=0, sticky='nsew')
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)
-        self.container.pack_propagate(False)
+        self.container.grid_columnconfigure(1, weight=3)
+        self.container.grid_propagate(False)
 
-        # nav-menu
-        self.nav_menu = tk.Frame(self.container, bg='#234235', width=w_width/4, height=w_height)
-        self.nav_menu.grid(row=0, column=0, sticky='nsew')
-        self.nav_menu.grid_rowconfigure((0, 2), weight=1)
-        self.nav_menu.grid_rowconfigure(1, weight=5)
-        self.nav_menu.grid_columnconfigure(0, weight=1)
-        self.nav_menu.pack_propagate(False)
+        # left_bar
+        self.left_bar = tk.Frame(self.container, bg='#234235')
+        self.left_bar.grid(column=0, row=0, padx=0, pady=0, sticky='nsew')
+        self.left_bar.grid_propagate(False)
 
-        ## button
-        self.filter_input = tk.Entry(self.nav_menu)
-        self.filter_input.insert(1, 'filter conversations...')
-        self.filter_input.grid(column=0, row=1, padx=10, pady=5, sticky='nsew')
+        ## entry frame
+        self.l_entry_frame = tk.Frame(self.left_bar)
+
+        ## conversations list frame
+        self.l_conv_list_frame = tk.Frame(self.left_bar)
+
+        ## create new conversation frame
+        self.l_new_conv_btn_frame = tk.Frame(self.left_bar)
 
 
+        # main_content
+        self.main_content = tk.Frame(self.container, bg='#C4C4C4')
+        self.main_content.grid(column=1, row=0, padx=0, pady=0, sticky='nsew')
+        self.main_content.grid_propagate(False)
 
-        ## frame
-
-        # main
-        self.main_content = tk.Frame(self.container, bg='#C4C4C4', width=w_width/4*3, height=w_height)
-        self.nav_menu.grid(row=0, column=0, sticky='nsew')
-
-        self.conversation_window = tk.Frame(self.main_content, bg='#ffffff')
-
-        self.message_window = tk.Frame(self.main_content, bg='#ffffff')
+        ##
     
     
